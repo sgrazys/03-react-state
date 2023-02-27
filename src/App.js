2d4754aef7f81ee0db09df557cb088f2aaff8149
sgrazys <s.grazys@gmail.com>
2023-02-27 11:34:51 +0200

    React-state. Home task WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [shape, setShape] = useState(true);
+
+	const randNum = () => Math.floor(Math.random() * (25 - 5 + 1)) + 5;
+	const [num, setNum] = useState(randNum());
+
+	const [number, setNumber] = useState(0);
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<div
+					style={{
+						border: '2px solid green',
+						width: '100px',
+						height: '100px',
+						borderRadius: shape ? '50%' : '0%',
+						marginBottom: '20px',
+						lineHeight: `100px`,
+					}}
+				>
+					{number}
+				</div>
+				<button onClick={() => setShape((e) => !e)}>
+					Change shape
+				</button>
+				<button onClick={() => setNum(randNum)}>Random</button>
+
+				<button onClick={() => setNumber((e) => e + 1)}>+</button>
+				<button onClick={() => setNumber((e) => e - 3)}>-</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
